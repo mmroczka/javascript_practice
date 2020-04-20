8a12c0b370600a4a09fc453a3481d67e203f65f3
Michael Mroczka <mmroczka@salesforce.com>
2020-04-20 17:59:12 -0400

    Add closure solution to apdx B exercise

--- YDKJS_get_started_appendix_B.js
+++ YDKJS_get_started_appendix_B.js
@@ -33,10 +33,41 @@ function scheduleMeeting(startTime, durationMinutes) {
   return true;
 }
 
-console.log(scheduleMeeting("7:00", 15)); // false
-console.log(scheduleMeeting("07:15", 30)); // false
-console.log(scheduleMeeting("7:30", 30)); // true
-console.log(scheduleMeeting("11:30", 60)); // true
-console.log(scheduleMeeting("17:00", 45)); // true
-console.log(scheduleMeeting("17:30", 30)); // false
-console.log(scheduleMeeting("18:00", 15)); // false
+// console.log(scheduleMeeting("7:00", 15)); // false
+// console.log(scheduleMeeting("07:15", 30)); // false
+// console.log(scheduleMeeting("7:30", 30)); // true
+// console.log(scheduleMeeting("11:30", 60)); // true
+// console.log(scheduleMeeting("17:00", 45)); // true
+// console.log(scheduleMeeting("17:30", 30)); // false
+// console.log(scheduleMeeting("18:00", 15)); // false
+
+// Practicing Closure
+/*
+The range(..) function takes a number as its first argument, representing the first number in a desired range of 
+numbers. The second argument is also a number representing the end of the desired range (inclusive). If the second 
+argument is omitted, then another function should be returned that expects that argument.
+*/
+
+function range(start, end) {
+  if (end !== undefined) {
+    let arr = [];
+    for (let i = start; i <= end; i++) arr.push(i);
+    return arr;
+  }
+  return (end) => {
+    let arr = [];
+    for (let i = start; i <= end; i++) arr.push(i);
+    return arr;
+  };
+}
+
+console.log(range(3, 3)); // [3]
+console.log(range(3, 8)); // [3,4,5,6,7,8]
+console.log(range(3, 0)); // []
+
+var start3 = range(3);
+var start4 = range(4);
+console.log(start3(3)); // [3]
+console.log(start3(8)); // [3,4,5,6,7,8]
+console.log(start3(0)); // []
+console.log(start4(6)); // [4,5,6]
